24efe3c248fb171916f8b5ed183e6d876d0d9e57
herkinlj <127352362+herkinlj@users.noreply.github.com>
2024-02-17 00:02:42 -0500

    Create 2k_test.sh

---
+++ 2k_test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Compile the program
+gcc -g -O3 --std=c99 -fopenmp -Wall -o par_gauss par_gauss.c
+
+# Number of threads to test
+THREADS="1 2 4 8 16 32 64 128"
+
+# Input file (change this to your input file or size)
+INPUT=2000
+
+# Run the program with different numbers of threads
+for THREAD_COUNT in $THREADS; do
+    export OMP_NUM_THREADS=$THREAD_COUNT
+    echo "Running with $THREAD_COUNT threads:"
+    srun ./par_gauss $INPUT
+    echo ""
+done
